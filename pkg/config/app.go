be14bbd77ccf4784d3ace629bb0fa84b53ed2f6b
Siddharth9 Jain <Siddharth9.Jain@ril.com>
2024-11-16 02:01:42 +0530

    final commit

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -4,26 +4,20 @@ import (
 	"log"
 
 	"github.com/jinzhu/gorm"
-	"github.com/jinzu/gorm"
-	_ "github.com/jinzu/gorm/dialects/mysql"
 )
 
 var (
 	db *gorm.DB
 )
 
-func Connect(){
+func Connect() {
 	d, err := gorm.Open("mysql", "siddharth:Siddharth@123/simplerest?charset=utf8&parseTime=True&loc=Local")
-	if err!= nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 	db = d
 }
 
-func GetDB() *form.DB{
+func GetDB() *gorm.DB {
 	return db
 }
-
-
-
-
--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -6,10 +6,10 @@ import (
 )
 
 var RegisterBookStoreRoutes = func(router *mux.Router) {
-	router.HandleFunc("/book/", controller.CreateBook).Methods("POST")
-	router.HandleFunc("/book/", controller.GetBook).Methods("GET")
-	router.HandleFunc("/book/{bookId}", controller.GetBookById).Methods("GET")
-	router.HandleFunc("/book/{bookId}", controller.UpdateBook).Methods("PUT")
-	router.HandleFunc("/book/{bookId}", controller.DeleteBook).Methods("DELETE")
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 
 }
